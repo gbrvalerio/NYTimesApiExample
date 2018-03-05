26efe740a75bb59f86087064364eea5da61f91ed
Gabriel Bezerra Valério <gbrvalerio@gmail.com>
2018-03-05 06:03:30 -0300

    Configuring workspace

---
+++ NyTimesMostPopular/Core/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  NyTimesMostPopular
+//
+//  Created by Gabriel Bezerra Valério on 05/03/18.
+//  Copyright © 2018 Particular. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    ///Default injected window
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ NyTimesMostPopular/Screens/News/View/NewsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NewsViewController.swift
+//  NyTimesMostPopular
+//
+//  Created by Gabriel Bezerra Valério on 05/03/18.
+//  Copyright © 2018 Particular. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
